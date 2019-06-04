e3e0e8d0e2650096b77406b93c04fc0ed1d9f925
Lucas Aubron <lucas.aubron@etu.unice.fr>
2019-06-04 17:00:01 +0200

    #9 4 listes JSON différents accessibles avec /lx avec x allant de 1 à 4

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
-const ClientRouter = require('./clients');
+const ClientRouteur1 = require('./clients/index1');
+const ClientRouteur2 = require('./clients/index2');
+const ClientRouteur3 = require('./clients/index3');
+const ClientRouteur4 = require('./clients/index4');
 
 const router = new Router();
 router.get('/status', (req, res) => res.status(200).json('ok'));
-router.use('/clients', ClientRouter);
+router.use('/clients/l1', ClientRouteur1);
+router.use('/clients/l2', ClientRouteur2);
+router.use('/clients/l3', ClientRouteur3);
+router.use('/clients/l4', ClientRouteur4);
 
 module.exports = router;
--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,5 +1,11 @@
-const Client = require('./client.model.js');
+const Client1 = require('./client1.model.js');
+const Client2 = require('./client2.model.js');
+const Client3 = require('./client3.model.js');
+const Client4 = require('./client4.model.js');
 
 module.exports = {
-  Client,
+  Client1,
+  Client2,
+  Client3,
+  Client4
 };

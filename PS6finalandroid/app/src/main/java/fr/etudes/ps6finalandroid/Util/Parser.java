ec06ff1f2d0c8f8edd95f6bd5bc1456f56fe45a5
kali cops <gabriel.plante@live.fr>
2019-06-04 20:10:30 +0200

    #10 Les données sont gardées en mémoires

---
+++ PS6finalandroid/app/src/main/java/fr/etudes/ps6finalandroid/Util/Parser.java
@@ -0,0 +1,40 @@
+package fr.etudes.ps6finalandroid.Util;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Parser {
+    private String fileName;
+
+    public Parser(String fileName){
+        this.fileName = fileName;
+    }
+
+    public boolean write(byte[] bytes, Context context){
+        try {
+            FileOutputStream outputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+            outputStream.write(bytes);
+            outputStream.close();
+        }
+        catch (Exception e){
+            e.getStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public byte[] read(Context context, int byteArraySize){
+        byte[] bytes = new byte[byteArraySize];
+        try{
+            FileInputStream inputStream = context.openFileInput(fileName);
+            inputStream.read(bytes);
+        }
+        catch (Exception e){
+            e.getStackTrace();
+            //for (int i = 0; i != bytes.length; i++)
+        }
+        return bytes;
+    }
+}

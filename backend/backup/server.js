ac0cb5d5ebacd070b01bc67159ccd2ee8f76bc40
GabrielPlante <gabriel.plante@live.fr>
2019-06-06 18:12:35 +0200

    #13 Avancement back end ajout getPositionNumber

--- backend/backup/server.js
+++ backend/backup/server.js
@@ -8,16 +8,18 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.get('/ajouterNum', function(req, res){
+app.get('/ajouterNum', function(req, res){///ajouterNum?numero=
     console.log("Ajout de numéro");
     var num = req.query.numero;
     liste.push(num);
-});
-
-app.get('/getTailleListe', function(req, res){
+}).get('/getTailleListe', function(req, res){///getTailleListe
     console.log("Demande de la taille");
     res.json({"taille":liste.length})
-})
+}).get('/getPositionNum', function(req, res){///getPositionNum?numero=
+    console.log("Demande de position");
+    var numero = req.query.numero;
+    res.json({"position":liste.indexOf(numero)});
+});
 
 var server = app.listen(3000, function () {
     console.log("Express is running on port 3000");
